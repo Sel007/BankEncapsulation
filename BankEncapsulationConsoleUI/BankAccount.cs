910788d3503def6702215401c452461062b58d51
Sel007 <selina.08225.1@gmail.com>
2020-07-01 11:42:18 -0400

    Add project files.

---
+++ BankEncapsulationConsoleUI/BankAccount.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankEncapsulationConsoleUI
+{
+    public class BankAccount
+    {
+        private double _balance = 0;
+
+        public void Deposit(double amount)
+        {
+            Console.WriteLine($"Simulating a deposit of {amount, 0:c} to your account");
+            _balance = amount;
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+    }
+
+}
